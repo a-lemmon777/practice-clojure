4bb4eb677cd048da5bf5cdc2fb65577bffea1d3e
Emma Sax <saxxx027@morris.umn.edu>
2014-05-21 16:46:17 -0500

    Add another example

--- joy-of-clojure/src/joy_of_clojure/core.clj
+++ joy-of-clojure/src/joy_of_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns joy-of-clojure.core)
 
 ;; Given ["Aaron" "David" "Lemmon"] returns "Lemmon, Aaron David".
+;; Given [“Emma” “Heming” “Sax”] returns “Sax, Emma Heming”.
 (defn rearrange [whole-name]
   (let [[f-name m-name l-name] whole-name]
     (str l-name ", " f-name " " m-name)))
