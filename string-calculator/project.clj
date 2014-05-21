fec0332bed410bcbf9e9bf32ad3c5a78f622a241
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-05-21 10:16:42 -0500

    Initial commit

---
+++ string-calculator/project.clj
@@ -0,0 +1,8 @@
+(defproject string-calculator "0.1.0-SNAPSHOT"
+  :description "A string calculator"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [expectations "2.0.6"]]
+  :plugins [[lein-autoexpect "1.0"]])
